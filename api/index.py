5021d169a291b919768e0ee749a010ff053ee168
Rajesh8356 <project4uindia@gmail.com>
2025-11-17 17:18:10 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,5 @@
 from app import app
 
-# Vercel expects a handler function for serverless functions
 def handler(request, response):
     with app.app_context():
         return app(request, response)
-
-# Alternative approach that also works:
-# from app import app as application
